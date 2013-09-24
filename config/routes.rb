2007594ac5f7bc61a803bbca1f36da938698cacf
Surya Tripathi <raj.surya19@gmail.com>
2013-09-24 19:30:24 +0530

    GET or POST for comback :|

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :gateway do
     get '/:gateway_id/ebsin/:order_id' => 'ebsin#show',     :as => :ebsin
-    get '/ebsin/:id/comeback'          => 'ebsin#comeback', :as => :ebsin_comeback
+    match '/ebsin/:id/comeback'          => 'ebsin#comeback', :as => :ebsin_comeback
   end
 end
