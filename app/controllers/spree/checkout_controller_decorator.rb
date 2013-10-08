2dbc227830a3e3ae339d5c881a7285df00b77dd3
Siddesh <siddesh.sh@styletag.com>
2013-10-08 15:31:01 +0530

    Complet the order if order has 100% wallet amount without going to ebs gateway.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,10 +6,18 @@ Spree::CheckoutController.class_eval do
 
     def redirect_for_ebsin
       return unless params[:state] == "payment"
-      @payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-      if @payment_method && @payment_method.kind_of?(Spree::PaymentMethod::Ebsin)
-        @order.update_attributes(object_params)
-        redirect_to gateway_ebsin_path(:gateway_id => @payment_method.id, :order_id => @order.id)
+      if @order.total > 0.0
+        @payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
+        if @payment_method && @payment_method.kind_of?(Spree::PaymentMethod::Ebsin)
+          @order.update_attributes(object_params)
+          redirect_to gateway_ebsin_path(:gateway_id => @payment_method.id, :order_id => @order.id)
+        end
+      elsif @order.total == 0.0
+        @order.reload
+        @order.next
+        session[:order_id] = nil
+        @order.finalize!
+        redirect_to order_url(@order, {:checkout_complete => true, :token => @order.token}), :notice => I18n.t("payment_success")
       end
     end
 end
