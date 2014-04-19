6a64194adad20f79e3370fa1fc8e25ff3c6edccf
ramkumar <ram.kumar@styletag.com>
2014-04-19 14:43:34 +0530

    refferal credit on wallet purchase

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,4 +1,5 @@
 Spree::CheckoutController.class_eval do
+    include AffiliateCredits
 
   before_filter :redirect_for_ebsin, :only => :update
 
@@ -12,10 +13,17 @@ Spree::CheckoutController.class_eval do
           @order.update_attributes(object_params)
           redirect_to gateway_ebsin_path(:gateway_id => @payment_method.id, :order_id => @order.id)
         end
-      elsif @order.total == 0.0
+      elsif @order.total == 0.0 #wallet checkout
         @order.reload
         @order.next
         session[:order_id] = nil
+         #referal credits
+        if !Spree::Affiliate.where(user_id: spree_current_user.id).empty? && (@order.state == 'complete') && spree_current_user.orders.complete.count==1
+      sender=Spree::User.find(Spree::Affiliate.where(user_id: spree_current_user.id).first.partner_id)
+
+      #create credit (if required)
+      create_affiliate_credits(sender, spree_current_user, "purchase")
+      end
         #@order.finalize!
         redirect_to order_url(@order, {:checkout_complete => true, :token => @order.token}), :notice => I18n.t("payment_success")
       end
