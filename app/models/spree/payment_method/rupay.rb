cb73e9c71b9f8c8e0cebaf344b20dfe487d5fb1f
Ramkumar <dramkumarcse@gmail.com>
2014-09-19 10:50:09 +0530

    Rupay payment method

---
+++ app/models/spree/payment_method/rupay.rb
@@ -0,0 +1,23 @@
+class Spree::PaymentMethod::Rupay < Spree::PaymentMethod
+  attr_accessible :preferred_account_id, :preferred_url, :preferred_secret_key, :preferred_mode, :preferred_currency_code
+  
+  preference :account_id, :string
+  preference :url,        :string, :default =>  "https://secure.ebs.in/pg/ma/sale/pay/"
+  preference :secret_key, :string
+  preference :mode, :string
+  preference :currency_code, :string
+  attr_accessible :preferred_account_id, :preferred_url, :preferred_secret_key, :preferred_mode, :preferred_currency_code
+
+  def self.payment_option
+  "1300"
+  end
+    
+  def payment_profiles_supported?
+    false
+  end
+  
+  def payment_source_class
+    Ebsinfo
+  end
+
+end
